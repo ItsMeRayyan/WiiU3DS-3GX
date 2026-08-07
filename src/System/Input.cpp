52783e783975a5c8c83fc9e959aff057edb573e4
ItsMeRayyan <runstarray@gmail.com>
2026-08-08 00:32:30 +0300

    Add touch input handling to Input class

--- src/System/Input.cpp
+++ src/System/Input.cpp
@@ -6,13 +6,44 @@ bool Input::Initialize()
     captured = false;
     overlayToggleRequested = false;
 
+    touchDown = false;
+    touchHeld = false;
+    touchReleased = false;
+    previousTouchHeld = false;
+
+    touchX = 0;
+    touchY = 0;
+
     return true;
 }
 
 void Input::Update(Context& context)
 {
-    // Reset this every frame.
+    // Reset one-frame events.
     overlayToggleRequested = false;
+    touchDown = false;
+    touchReleased = false;
+
+    hidScanInput();
+
+    u32 held = hidKeysHeld();
+
+    bool currentTouchHeld = (held & KEY_TOUCH) != 0;
+
+    touchDown = currentTouchHeld && !previousTouchHeld;
+    touchHeld = currentTouchHeld;
+    touchReleased = !currentTouchHeld && previousTouchHeld;
+
+    if (currentTouchHeld)
+    {
+        touchPosition touch;
+        hidTouchRead(&touch);
+
+        touchX = touch.px;
+        touchY = touch.py;
+    }
+
+    previousTouchHeld = currentTouchHeld;
 
     /*
         TODO:
@@ -63,8 +94,41 @@ bool Input::IsCaptured() const
     return captured;
 }
 
+bool Input::IsTouchDown() const
+{
+    return touchDown;
+}
+
+bool Input::IsTouchHeld() const
+{
+    return touchHeld;
+}
+
+bool Input::IsTouchReleased() const
+{
+    return touchReleased;
+}
+
+int Input::GetTouchX() const
+{
+    return touchX;
+}
+
+int Input::GetTouchY() const
+{
+    return touchY;
+}
+
 void Input::Shutdown()
 {
     captured = false;
     overlayToggleRequested = false;
+
+    touchDown = false;
+    touchHeld = false;
+    touchReleased = false;
+    previousTouchHeld = false;
+
+    touchX = 0;
+    touchY = 0;
 }
