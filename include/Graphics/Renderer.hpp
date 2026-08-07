dceea342a7486edabfb05e1b4c62ba145a019ee5
ItsMeRayyan <runstarray@gmail.com>
2026-08-07 23:35:50 +0300

    Add DrawRect method to Renderer class

--- include/Graphics/Renderer.hpp
+++ include/Graphics/Renderer.hpp
@@ -1,13 +1,23 @@
+
 #pragma once
 
 #include "Core/Context.hpp"
+#include "Graphics/Color.hpp"
+#include "Graphics/Rect.hpp"
 
 class Renderer
 {
 public:
     static bool Initialize(Context& context);
+
     static void BeginFrame(Context& context);
     static void EndFrame(Context& context);
+
+    static void DrawRect(
+        const Rect& rect,
+        const Color& color
+    );
+
     static void Shutdown(Context& context);
 
 private:
