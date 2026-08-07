29b518dc39a06e0c4c5a23828265e00cfeffd6f0
ItsMeRayyan <runstarray@gmail.com>
2026-08-07 23:32:25 +0300

    Implement basic Renderer class structure

---
+++ src/Graphics/Renderer.cpp
@@ -0,0 +1,22 @@
+#include "Graphics/Renderer.hpp"
+
+bool Renderer::Initialize(Context& context)
+{
+    // TODO: initialize graphics resources
+    return true;
+}
+
+void Renderer::BeginFrame(Context& context)
+{
+    // TODO: prepare frame for drawing
+}
+
+void Renderer::EndFrame(Context& context)
+{
+    // TODO: finish/present frame
+}
+
+void Renderer::Shutdown(Context& context)
+{
+    // TODO: clean up graphics resources
+}
